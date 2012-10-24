1b12e128ee4856b0cc0cced56ea37b866d4ec76a
Christopher Nabors <PillarDD@Gmail.com>
2012-10-24 19:54:11 -0400

    Half time on fade

--- static/js/bethany.js
+++ static/js/bethany.js
@@ -50,14 +50,14 @@ function homeNavBoxEvents(){
         // Adjust Content                                                     \\
         //====================================================================\\
         // Initial Fade
-        $('home_nav_content').fade(0);
+        $('home_nav_content').set('tween', { duration: 500 }).fade('out');
         // Replace Content, then Fade back in on a timer.
         setTimeout(function(){
             // Replace Content
             $('home_nav_content').innerHTML = "This is the test content of home. This is the test content of home. This is the test content of home. This is the test content of home. This is the test content of home. This is the test content of home. This is the test content of home. This is the test content of home. This is the test content of home. This is the test content of home.";
             // Fade back in
-            $('home_nav_content').fade(1); 
-        }, 1000);
+            $('home_nav_content').set('tween', { duration: 500 }).fade('in'); 
+        }, 500);
     });
     /**************************************************************************\
      * ABOUT                                                                  *
@@ -87,14 +87,14 @@ function homeNavBoxEvents(){
         // Adjust Content                                                     \\
         //====================================================================\\
         // Initial Fade
-        $('home_nav_content').fade(0);
+        $('home_nav_content').set('tween', { duration: 500 }).fade('out');
         // Replace Content, then Fade back in on a timer.
         setTimeout(function(){
             // Replace Content
             $('home_nav_content').innerHTML = "This is the test content of about. This is the test content of about. This is the test content of about. This is the test content of about. This is the test content of about. This is the test content of about. This is the test content of about. This is the test content of about. This is the test content of about. This is the test content of about.";
             // Fade back in
-            $('home_nav_content').fade(1); 
-        }, 1000);
+            $('home_nav_content').set('tween', { duration: 500 }).fade('in'); 
+        }, 500);
     });
     /**************************************************************************\
      * RESUME                                                                 *
@@ -124,14 +124,14 @@ function homeNavBoxEvents(){
         // Adjust Content                                                     \\
         //====================================================================\\
         // Initial Fade
-        $('home_nav_content').fade(0);
+        $('home_nav_content').set('tween', { duration: 500 }).fade('out');
         // Replace Content, then Fade back in on a timer.
         setTimeout(function(){
             // Replace Content
             $('home_nav_content').innerHTML = "This is the test content of resume. This is the test content of resume. This is the test content of resume. This is the test content of resume. This is the test content of resume. This is the test content of resume. This is the test content of resume. This is the test content of resume. This is the test content of resume. This is the test content of resume.";
             // Fade back in
-            $('home_nav_content').fade(1); 
-        }, 1000);
+            $('home_nav_content').set('tween', { duration: 500 }).fade('in'); 
+        }, 500);
     });
     /**************************************************************************\
      * CONTACT                                                                *
@@ -161,13 +161,14 @@ function homeNavBoxEvents(){
         // Adjust Content                                                     \\
         //====================================================================\\
         // Initial Fade
-        $('home_nav_content').fade(0);
+        $('home_nav_content').set('tween', { duration: 500 }).fade('out');
         // Replace Content, then Fade back in on a timer.
         setTimeout(function(){
             // Replace Content
             $('home_nav_content').innerHTML = "This is the test content of contact. This is the test content of contact. This is the test content of contact. This is the test content of contact. This is the test content of contact. This is the test content of contact. This is the test content of contact. This is the test content of contact. This is the test content of contact. This is the test content of contact.";
             // Fade back in
             $('home_nav_content').fade(1); 
-        }, 1000);
+            $('home_nav_content').set('tween', { duration: 500 }).fade('in'); 
+        }, 500);
     });
 }
